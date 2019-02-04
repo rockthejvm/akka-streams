9d657d889c826fffe0c5f026651bb4729eddf00a
Daniel Ciocirlan <daniel.ciocirlan@gmail.com>
2019-02-04 00:43:05 +0000

    finale

--- src/main/scala/part5_advanced/CustomOperators.scala
+++ src/main/scala/part5_advanced/CustomOperators.scala
@@ -1,12 +1,13 @@
 package part5_advanced
 
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Sink, Source}
+import akka.stream.scaladsl.{Flow, Keep, Sink, Source}
 import akka.stream._
-import akka.stream.stage.{GraphStage, GraphStageLogic, InHandler, OutHandler}
+import akka.stream.stage._
 
 import scala.collection.mutable
-import scala.util.Random
+import scala.concurrent.{Future, Promise}
+import scala.util.{Failure, Random, Success}
 
 object CustomOperators extends App {
 
@@ -89,5 +90,111 @@ object CustomOperators extends App {
   }
 
   val batcherSink = Sink.fromGraph(new Batcher(10))
-  randomGeneratorSource.to(batcherSink).run()
+  // randomGeneratorSource.to(batcherSink).run()
+
+  /**
+    * Exercise: a custom flow - a simple filter flow
+    * - 2 ports: an input port and an output port
+    */
+
+  class SimpleFilter[T](predicate: T => Boolean) extends GraphStage[FlowShape[T, T]] {
+
+    val inPort = Inlet[T]("filterIn")
+    val outPort = Outlet[T]("filterOut")
+
+    override def shape: FlowShape[T, T] = FlowShape(inPort, outPort)
+
+    override def createLogic(inheritedAttributes: Attributes): GraphStageLogic = new GraphStageLogic(shape) {
+      setHandler(outPort, new OutHandler {
+        override def onPull(): Unit = pull(inPort)
+      })
+
+      setHandler(inPort, new InHandler {
+        override def onPush(): Unit = {
+          try {
+            val nextElement = grab(inPort)
+
+            if (predicate(nextElement)) {
+              push(outPort, nextElement) // pass it on
+            } else {
+              pull(inPort) // ask for another element
+            }
+          } catch {
+            case e: Throwable => failStage(e)
+          }
+        }
+      })
+    }
+  }
+
+  val myFilter = Flow.fromGraph(new SimpleFilter[Int](_ > 50))
+  // randomGeneratorSource.via(myFilter).to(batcherSink).run()
+  // backpressure OOTB!!!!!
+
+  /**
+    * Materialized values in graph stages
+    */
+
+  // 3 - a flow that counts the number of elements that go through it
+  class CounterFlow[T] extends GraphStageWithMaterializedValue[FlowShape[T, T], Future[Int]] {
+
+    val inPort = Inlet[T]("counterInt")
+    val outPort = Outlet[T]("counterOut")
+
+    override val shape = FlowShape(inPort, outPort)
+
+    override def createLogicAndMaterializedValue(inheritedAttributes: Attributes): (GraphStageLogic, Future[Int]) = {
+
+      val promise = Promise[Int]
+      val logic = new GraphStageLogic(shape) {
+        // setting mutable state
+        var counter = 0
+
+        setHandler(outPort, new OutHandler {
+          override def onPull(): Unit = pull(inPort)
+
+          override def onDownstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onDownstreamFinish()
+          }
+        })
+
+        setHandler(inPort, new InHandler {
+          override def onPush(): Unit = {
+            // extract the element
+            val nextElement = grab(inPort)
+            counter += 1
+            // pass it on
+            push(outPort, nextElement)
+          }
+
+          override def onUpstreamFinish(): Unit = {
+            promise.success(counter)
+            super.onUpstreamFinish()
+          }
+
+          override def onUpstreamFailure(ex: Throwable): Unit = {
+            promise.failure(ex)
+            super.onUpstreamFailure(ex)
+          }
+        })
+      }
+
+      (logic, promise.future)
+    }
+  }
+
+  val counterFlow = Flow.fromGraph(new CounterFlow[Int])
+  val countFuture = Source(1 to 10)
+    // .map(x => if (x == 7) throw new RuntimeException("gotcha!") else x)
+    .viaMat(counterFlow)(Keep.right)
+    .to(Sink.foreach(x => if (x == 7) throw new RuntimeException("gotcha, sink!") else println(x)))
+    // .to(Sink.foreach[Int](println))
+    .run()
+
+  import system.dispatcher
+  countFuture.onComplete {
+    case Success(count) => println(s"The number of elements passed: $count")
+    case Failure(ex) => println(s"Counting the elements failed: $ex")
+  }
 }
